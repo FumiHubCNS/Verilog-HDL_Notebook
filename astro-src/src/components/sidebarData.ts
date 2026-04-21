b3f480b36fb003a8861e3395490b3dd3168ece51
FumiHubCNS <fendo@cns.s.u-tokyo.ac.jp>
2026-04-21 16:12:55 +0900

    [fix] link

--- astro-src/src/components/sidebarData.ts
+++ astro-src/src/components/sidebarData.ts
@@ -4,77 +4,60 @@ export type NavItem = {
   children?: NavItem[];
 };
 
+const base = import.meta.env.BASE_URL;
+
 export const navItems: NavItem[] = [
   ////////////////////////////////////////////////////////////
   // Home
   ////////////////////////////////////////////////////////////
   {
     title: "Home",
-    href: "/",
+    href: `${base}`,
   },
   ////////////////////////////////////////////////////////////
-  // notebook 
+  // notebook
   ////////////////////////////////////////////////////////////
   {
     title: "notebook",
-    href: "/notebook/",
+    href: `${base}notebook/`,
     children: [
       {
         title: "Open-It",
-        href: "/notebook/open-it/",
+        href: `${base}notebook/open-it/`,
         children: [
           ////////////////////////////////////////////////////////////
           {
             title: "Counter",
-            href: "/notebook/open-it/counter/",
+            href: `${base}notebook/open-it/counter/`,
           },
           ////////////////////////////////////////////////////////////
           {
             title: "Counter v2",
-            href: "/notebook/open-it/counter-v2/",
+            href: `${base}notebook/open-it/counter-v2/`,
           },
           ////////////////////////////////////////////////////////////
           {
             title: "Flip Flop",
-            href: "/notebook/open-it/flip-flop/",
+            href: `${base}notebook/open-it/flip-flop/`,
           },
           ////////////////////////////////////////////////////////////
           {
             title: "Flip Flop v2",
-            href: "/notebook/open-it/flip-flop-v2/",
+            href: `${base}notebook/open-it/flip-flop-v2/`,
           },
           ////////////////////////////////////////////////////////////
           {
             title: "Shift Register",
-            href: "/notebook/open-it/shift-register/",
+            href: `${base}notebook/open-it/shift-register/`,
           },
           /////////////////////////////////////////////////////////////
           {
             title: "State Machine",
-            href: "/notebook/open-it/state-machine/",
+            href: `${base}notebook/open-it/state-machine/`,
           },
           /////////////////////////////////////////////////////////////
         ],
       },
-      ////////////////////////////////////////////////////////////
-      // Hoge
-      ////////////////////////////////////////////////////////////
-      // {
-      //   title: "Examples",
-      //   href: "/tutorials/examples/",
-      //   children: [
-      //     {
-      //       title: "Counter",
-      //       href: "/tutorials/examples/counter/",
-      //     },
-      //   ],
-      // },
-      ////////////////////////////////////////////////////////////
     ],
   },
-
-  // {
-  //   title: "Tools",
-  //   href: "/tools/",
-  // },
 ];
