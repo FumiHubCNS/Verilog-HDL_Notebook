78a890c60cd648d80baf2fd77b40c23713b2bc7a
FumiHubCNS <fendo@cns.s.u-tokyo.ac.jp>
2026-04-21 15:45:29 +0900

    [update] astro.config.mjs

--- astro-src/astro.config.mjs
+++ astro-src/astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
+// astro-src/astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://fumihubcns.github.io',
+  base: '/Verilog-HDL_Notebook/',
+});
